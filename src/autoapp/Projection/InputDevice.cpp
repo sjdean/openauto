8d2c50e96d54038a2a69e2fba20a76d3971acb79
Simon Dean <sjdean@blargle.co.uk>
2025-12-08 16:42:23 +0000

    Restore Limited set of Button Codes

--- src/autoapp/Projection/InputDevice.cpp
+++ src/autoapp/Projection/InputDevice.cpp
@@ -1,6 +1,10 @@
 #include <f1x/openauto/autoapp/Projection/IInputDeviceEventHandler.hpp>
 #include <f1x/openauto/autoapp/Projection/InputDevice.hpp>
 #include <qloggingcategory.h>
+#include <service/control/message/AudioFocusNotification.pb.h>
+#include <service/inputsource/message/KeyEvent.pb.h>
+#include <service/media/shared/message/Setup.pb.h>
+#include <shared/MessageStatus.pb.h>
 Q_LOGGING_CATEGORY(lcInput, "journeyos.input")
 
 namespace f1x::openauto::autoapp::projection {
@@ -190,8 +194,48 @@ namespace f1x::openauto::autoapp::projection {
   }
 
   IInputDevice::ButtonCodes InputDevice::getSupportedButtonCodes() const {
-    // TODO: Restore BUtton Codes
-    // return configuration_->getButtonCodes();
-  }
+      std::vector<aap_protobuf::service::media::sink::message::KeyCode> codes;
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_3D_MODE);
+
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_PRIMARY_BUTTON);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_SECONDARY_BUTTON);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_TERTIARY_BUTTON);
+
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_ROTARY_CONTROLLER);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_DPAD_CENTER);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_DPAD_LEFT);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_DPAD_RIGHT);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_DPAD_UP);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_DPAD_DOWN);
+
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_MEDIA_NEXT);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_MEDIA_PREVIOUS);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_MEDIA_PLAY);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_MEDIA_PLAY_PAUSE);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_MEDIA_STOP);
+
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_BACK);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_FORWARD);
+
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_BRIGHTNESS_UP);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_BRIGHTNESS_DOWN);
+
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_VOLUME_UP);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_VOLUME_DOWN);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_VOLUME_MUTE);
+
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_NAVIGATION);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_HOME);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_MUSIC);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_RADIO);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_SEARCH);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_CALL);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_ENDCALL);
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_TEL);
+
+      codes.push_back(aap_protobuf::service::media::sink::message::KEYCODE_MUTE);
+
+      return codes;
+    }
 
 }
