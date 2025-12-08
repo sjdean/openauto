672546bc6fcb3accba92806e45bbf4005e018bde
Simon Dean <sjdean@blargle.co.uk>
2025-12-08 15:58:46 +0000

    Remove Log.hpp - no longer required

--- include/f1x/openauto/Common/Log.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <boost/log/trivial.hpp>
-
-// TODO: Think about getting rid of OPENAUTO_LOG so we can switch to Qt logging.
-
-#define OPENAUTO_LOG_CONTEXT "" //"(" << typeid(*this).name() << "::" << __func__ << ")"
-#define OPENAUTO_LOG(severity) BOOST_LOG_TRIVIAL(severity) << "[OpenAuto] " << OPENAUTO_LOG_CONTEXT
